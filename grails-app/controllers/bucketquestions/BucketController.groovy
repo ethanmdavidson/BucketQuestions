b5ec9c4a1282aaffbdb4db66bde4f4fc86d88ac9
ethanmdavidson <ethanmdavidson@gmail.com>
2016-12-22 01:10:44 -0500

    forgot to add the render statement

--- grails-app/controllers/bucketquestions/BucketController.groovy
+++ grails-app/controllers/bucketquestions/BucketController.groovy
@@ -5,7 +5,7 @@ class BucketController {
 
     def index() {
         if(params.containsKey("code") && Bucket.countByCodeword(params.code) > 0){
-            
+            render(view:"index", model:[Bucket:Bucket.findByCodeword(params.code)])
         } else {
             //if no codeword is given, or the given codeword doesn't match an
             // existing bucket, redirect to main index
